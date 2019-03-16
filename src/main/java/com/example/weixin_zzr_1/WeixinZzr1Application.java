89d2b0975c012281cf01867a0483c3f8a32794f5
zzr <zzr@DESKTOP-1STASHV>
2019-03-16 16:32:29 +0800

    初次提交Spring Boot的项目模板

---
+++ src/main/java/com/example/weixin_zzr_1/WeixinZzr1Application.java
@@ -0,0 +1,13 @@
+package com.example.weixin_zzr_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeixinZzr1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeixinZzr1Application.class, args);
+	}
+
+}
