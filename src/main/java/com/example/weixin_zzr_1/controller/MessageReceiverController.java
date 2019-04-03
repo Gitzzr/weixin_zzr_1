89508a94f0e478616e7b7bb5acfad9a1ddc55d7f
zwy13631658682 <zwy13631658682@LAPTOP-QPI954N9>
2019-04-03 16:40:20 +0800

    4.3未完成等会做

--- src/main/java/com/example/weixin_zzr_1/controller/MessageReceiverController.java
+++ src/main/java/com/example/weixin_zzr_1/controller/MessageReceiverController.java
@@ -1,13 +1,28 @@
 package com.example.weixin_zzr_1.controller;
 
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.StringReader;
+
+import javax.xml.bind.JAXB;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.weixin_zzr_1.domain.InMessage;
+import com.example.weixin_zzr_1.service.MessageTypeMapper;
 
 // 控制器 : 负责接收用户的请求参数、调用业务逻辑层代码、返回视图/结果给客户端（浏览器）
 // @Controller  基于JSP的控制器
@@ -47,6 +62,27 @@ public class MessageReceiverController {
 				@RequestBody String xml) {
 			LOG.debug("收到用户发送给公众号的信息: \n-----------------------------------------\n"
 					+ "{}\n-----------------------------------------\n", xml);
+			
+//			if (xml.contains("<MsgType><![CDATA[text]]></MsgType>")) {
+//			} else if (xml.contains("<MsgType><![CDATA[image]]></MsgType>")) {
+//			} else if (xml.contains("<MsgType><![CDATA[voice]]></MsgType>")) {
+//			} else if (xml.contains("<MsgType><![CDATA[video]]></MsgType>")) {
+//			} else if (xml.contains("<MsgType><![CDATA[location]]></MsgType>")) {
+//			}
+
+			// 截取消息类型
+			// <MsgType><![CDATA[text]]></MsgType>
+			String type = xml.substring(xml.indexOf("<MsgType><![CDATA[") + 18);
+			type = type.substring(0, type.indexOf("]]></MsgType>"));
+
+			Class<InMessage> cla = MessageTypeMapper.getClass(type);
+
+			// 使用JAXB完成XML转换为Java对象的操作
+			InMessage inMessage = JAXB.unmarshal(new StringReader(xml), cla);
+
+			LOG.debug("转换得到的消息对象 \n{}\n", inMessage.toString());
+			
+			
 			// 由于后面会把消息放入队列中，所以这里直接返回success。
 			return "success";
 		}
